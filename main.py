fbf078a2dc640344ec1d4562f5e1372372ff0750
Baltimon <johannes.beni@gmail.com>
2023-10-16 03:48:54 +0200

    ADD coments in main

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from random import random
 
-#name of the File with the wirds
+# name of the File with the wirds
 filename="wordslist.txt"
 
 
-#prints the Menu
+# prints the Menu
 def menu():
   print("\nHello and Welcome to Hangman by Baltimon \n")
   print("1. START Game")
@@ -12,11 +12,10 @@ def menu():
   print("3. END Game")
 
 
-
-
-# prints how many Loves are left
+# prints how many Lives are left
 def printLives(live):
   print("You have ", live, "  left!!")
+
   
 # Checks if the word is guest completely  
 def testWin(solution):
@@ -26,8 +25,9 @@ def testWin(solution):
     if (solution[i]==" _ "):
       return False
   return win
+
   
-#Loads al words from the File and returns one random word
+# Loads al words from the File and returns one random word
 def selectWord():
   file=open(filename,"r+")
   words=file.readlines()
@@ -40,15 +40,13 @@ def selectWord():
   return listedword
 
 
-
-# runns the game1
+# contains game loop with game logic and user-interaction during the game
 def game():
   live=10
   word=selectWord()
   solution=[]
   for i in range(len(word)):
     solution.append(" _ ")
-  
   while(live > 0 and not testWin(solution)):
     print("Guess the Letters ")
     printLives(live)
@@ -60,21 +58,22 @@ def game():
         solution[i]= word[i]
         found=True
     if(found==False):
-      live=live-1  
-    
+      live=live-1     
   if(testWin(solution)):
     print("Your Won !!!")
     print(solution)
   else:
     print(" You Lost :(")
+
   
-#Askes payer for a word and adds it to the List in the file  
+# Askes payer for a word and adds it to the List in the file  
 def addWord(word):
   file=open(filename,"a+")
   file.write("\n"+word)
   file.close
   
-#prints al words in the file
+
+# prints al words in the file
 def printFile():
   file=open(filename,"r+")
   words=file.readlines()
@@ -83,15 +82,13 @@ def printFile():
   print(words)
   file.close
 
-
-
+# main function. this is going to be executed when starting the program
 def main():
   while(True):
     menu()
     x= int(4)
     while(x != 1 and x != 2 and x != 3):
-      x= int(input("What do you want to do ?"))
-  
+      x= int(input("What do you want to do ?")) 
     if (x==1):
       game()
     if (x==2):
@@ -100,8 +97,6 @@ def main():
       print("Your list is now ")
       addWord(wrd)
       printFile()
-      #TODO
-      #neue Methode adden die Wort in die datei speichert
     if (x==3):
       print("\nThe END\n\ngood bye\n")
       break
